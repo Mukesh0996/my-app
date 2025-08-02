f22e1546155c77bd7ff5ec4fdbf4dc37d0d3e13e
Mukesh0996 <34233598+Mukesh0996@users.noreply.github.com>
2025-08-02 18:19:19 +0530

    Added steps logic to go back and forth

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import progressbarcss from './ProgressBar.module.css';
+import { AppContext } from '../../utils/context';
 
 const ProgressBar = () => {
 
+  const { step  } = useContext(AppContext);
+ 
   return  <div className ={progressbarcss.container}>  
-           <div className={progressbarcss.percent}></div>
+            <div style={{ width: `${ step * 25  }%` }} className={progressbarcss.percent}> </div>
          </div>
 }
 export default ProgressBar;
--- src/components/SignatureGenerator/SignatureGenerator.js
+++ src/components/SignatureGenerator/SignatureGenerator.js
@@ -16,11 +16,36 @@ const SignatureGenerator = () => {
         return updatedFields;        
        })
     }
-    return <div className={signatureGeneratorStyles.container}> {
 
-            Object.entries(fields[step]).map((field, key) => <Field key={key} name={field[0]} onChangeHandler={onChangeHandler} { ...field[1] }/> )
+    const onStepChange = (value) => {
 
-            } </div>
+        if( step == 4 && value == 1  ) {
+            
+            return;
+        }
+        setStepValue(prevValue => prevValue + value);
+    }
+
+    return <div className={signatureGeneratorStyles.container}> 
+                {
+                    Object.entries(fields[step]).map((field, key) => <Field key={key} name={field[0]} onChangeHandler={onChangeHandler} { ...field[1] }/> )
+                } 
+                <div className={signatureGeneratorStyles.buttons}>
+                    <button 
+                        onClick={()=> { onStepChange(-1) }} 
+                        disabled={ step == 1 ? true : false } 
+                        className={signatureGeneratorStyles.previous}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left mr-1 h-4 w-4"><path d="m15 18-6-6 6-6"></path></svg>
+                        Previous
+                    </button>
+                    <button 
+                        onClick={()=> { onStepChange(1) }}  
+                        className={signatureGeneratorStyles.next}> 
+                        Next
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right ms-1 h-4 w-4"><path d="m9 18 6-6-6-6"></path></svg>
+                    </button>
+                </div>
+            </div>
 }
 
 export default SignatureGenerator;
