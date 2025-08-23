cd7061e6cec82e13f25903f7a28f0cb661fecb09
Mukesh0996 <34233598+Mukesh0996@users.noreply.github.com>
2025-08-23 11:40:21 +0530

    final changes

--- src/components/SignatureGenerator/SignatureGenerator.js
+++ src/components/SignatureGenerator/SignatureGenerator.js
@@ -33,7 +33,7 @@ const SignatureGenerator = () => {
                 <div className={signatureGeneratorStyles.buttons}>
                     <button 
                         onClick={()=> { onStepChange(-1) }} 
-                        disabled={ step == 1 ? true : false } 
+                        disabled={ step === 1 ? true : false } 
                         className={signatureGeneratorStyles.previous}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left mr-1 h-4 w-4"><path d="m15 18-6-6 6-6"></path></svg>
                         Previous
